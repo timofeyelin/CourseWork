df74fff9037f37444bfe0971f337ff42e0ab587a
TimofeyElin <elintimofey05@gmail.com>
2025-11-04 18:25:59 +0300

    CW-8

--- Backend/Backend.Api/Program.cs
+++ Backend/Backend.Api/Program.cs
@@ -1,12 +1,25 @@
+using Backend.Application.Interfaces;
+using Backend.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IAppDbContext, AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
