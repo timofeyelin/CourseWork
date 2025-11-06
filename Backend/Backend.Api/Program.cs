24d79488de02322c7b8b8a43e76b11f13511029f
TimofeyElin <elintimofey05@gmail.com>
2025-11-06 23:03:12 +0300

    Fix https

--- Backend/Backend.Api/Program.cs
+++ Backend/Backend.Api/Program.cs
@@ -86,7 +86,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("AllowFrontend");
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
